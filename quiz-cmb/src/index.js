c09ea458f2b708b435e9644a2392b0764566d523
vrsmarcos00 <vrs_marcos@hotmail.com>
2023-09-26 20:24:06 -0300

    chore: Adiciona lógica para página de questões

--- quiz-cmb/src/index.js
+++ quiz-cmb/src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import WaitingRoom from './pages/WaitingRoom'
+import WaitingRoom from './pages/WaitingRoom';
+import QuestionStudent from './pages/QuestionStudent';
+import QuestionPresenter from './pages/QuestionPresenter';
 
 ReactDOM.render(
   <Router>
     <Routes>
       <Route path="/home" element={<Home />} />
       <Route path="/waiting-room" element={<WaitingRoom />} />
+      <Route path="/question-student" element={<QuestionStudent />} />
+      <Route path="/question-presenter" element={<QuestionPresenter />} />
     </Routes>
   </Router>,
   document.getElementById('root')
