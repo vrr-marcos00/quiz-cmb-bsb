a3ede6c80054fc09b76906e352a553b8a29ab2cc
brenoxavier48 <brenoxavier8.bx@gmail.com>
2023-09-28 20:51:09 -0300

    feat: init server events control

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,22 @@
-const http = require('http');
-const express = require('express');
-const socketIo = require('socket.io');
-const fs = require('fs');
-const path = require('path');
+const http = require("http");
+const express = require("express");
+const socketIo = require("socket.io");
+const fs = require("fs");
+const path = require("path");
 
-const questions = require('./database/questions.json');
+const questions = require("./database/questions.json");
+
+const {
+  setNextQuestion,
+  getCurrentQuestion,
+  getCurrentPhase,
+  updateToNextLevel,
+  getClientGameState,
+  calculatePointsAndRestartUsersCurrentAnswers,
+  updateUserAnswer,
+  getCurrentUserAnswer,
+  initQuiz,
+} = require("./usecases/game.js");
 
 // Cria o aplicativo Express e o servidor HTTP
 const app = express();
@@ -13,21 +25,20 @@ const server = http.createServer(app);
 // Cria o servidor Socket.io e associe-o ao servidor HTTP
 const io = socketIo(server, {
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
+    origin: "*",
+    methods: ["GET", "POST"],
     credentials: true,
   },
 });
-
 
 // Objeto para rastrear as respostas e pontuações dos participantes
 const data = {
   participants: {},
   answeredQuestions: [],
-  currentPhase: 'easy',
+  currentPhase: "easy",
 };
 
-const phases = ['easy', 'medium', 'difficult'];
+const phases = ["easy", "medium", "difficult"];
 let currentPhaseIndex = 0;
 
 // Armazena o estado do jogo para cada cliente
@@ -41,31 +52,31 @@ const nextQuestionIndexes = {
 };
 
 // Objeto para rastrear as salas
-const room = require('./database/room.json') || { roomCode: '', users: [] };
+const room = require("./database/room.json") || { roomCode: "", users: [] };
 
 // Função para salvar os dados em um arquivo JSON
 function saveDataToJsonFile(data) {
   const jsonData = JSON.stringify(data, null, 2);
-  const filePath = path.join(__dirname, 'database', 'quizData.json');
+  const filePath = path.join(__dirname, "database", "quizData.json");
 
   fs.writeFile(filePath, jsonData, (err) => {
     if (err) {
-      console.error('Erro ao salvar os dados:', err);
+      console.error("Erro ao salvar os dados:", err);
     } else {
-      console.log('Dados salvos com sucesso em quizData.json');
+      console.log("Dados salvos com sucesso em quizData.json");
     }
   });
 }
 
 function saveRoomToFile() {
   const jsonData = JSON.stringify(room, null, 2);
-  const filePath = path.join(__dirname, 'database', 'room.json');
+  const filePath = path.join(__dirname, "database", "room.json");
 
   fs.writeFile(filePath, jsonData, (err) => {
     if (err) {
-      console.error('Erro ao salvar sala:', err);
+      console.error("Erro ao salvar sala:", err);
     } else {
-      console.log('Sala salva com sucesso em database/room.json');
+      console.log("Sala salva com sucesso em database/room.json");
     }
   });
 }
@@ -78,21 +89,21 @@ function generateRoomCode() {
 }
 
 // Manipulador de eventos para quando um cliente se conecta
-io.on('connection', (socket) => {
-  clientGameState[socket.id] = { phase: 'easy' };
+io.on("connection", (socket) => {
+  clientGameState[socket.id] = { phase: "easy" };
 
   // Manipulador de eventos para quando um cliente se desconecta
-  socket.on('disconnect', () => {
-    console.log('Um cliente se desconectou');
+  socket.on("disconnect", () => {
+    console.log("Um cliente se desconectou");
   });
 
   // Manipulador de eventos para desconectar um cliente quando um evento personalizado é acionado
-  socket.on('forceDisconnect', () => {
+  socket.on("forceDisconnect", () => {
     socket.disconnect();
     console.log(`Cliente desconectado: ${socket.id} - force disconnect`);
   });
 
-  socket.on('disconnectAllUsers', () => {
+  socket.on("disconnectAllUsers", () => {
     const connectedClients = io.of("/").sockets;
 
     if (room.users.length > 0) {
@@ -110,20 +121,20 @@ io.on('connection', (socket) => {
   });
 
   // Limpa arquivo romm
-  socket.on('clearFileRoom', () => {
-    room.roomCode = '';
+  socket.on("clearFileRoom", () => {
+    room.roomCode = "";
     room.users = [];
     saveRoomToFile();
 
-    socket.emit('clearFileRoomMessage', 'Arquivo room limpado!');
+    socket.emit("clearFileRoomMessage", "Arquivo room limpado!");
   });
 
-  socket.on('start-game', () => {
+  socket.on("start-game", () => {
     // Inicie o jogo com a primeira pergunta
-    io.emit('question', questions.easy[0]);
+    io.emit("question", questions.easy[0]);
   });
 
-  socket.on('next-question', () => {
+  socket.on("next-question", () => {
     const currentPhase = phases[currentPhaseIndex];
     const questionsForPhase = questions[currentPhase];
 
@@ -134,40 +145,41 @@ io.on('connection', (socket) => {
 
       if (nextQuestionIndexes[currentPhase] >= questionsForPhase.length) {
         // Todas as perguntas da fase atual foram exibidas
-        io.emit('phase-complete', currentPhase);
+        io.emit("phase-complete", currentPhase);
 
         // Avança para a próxima fase se houver
         currentPhaseIndex++;
         if (currentPhaseIndex < phases.length) {
           const nextPhase = phases[currentPhaseIndex];
-          io.emit('next-phase', nextPhase);
+          io.emit("next-phase", nextPhase);
           nextQuestionIndexes[nextPhase] = 0;
           if (questions[nextPhase].length > 0) {
-            io.emit('question', questions[nextPhase][0]);
+            io.emit("question", questions[nextPhase][0]);
           } else {
-            io.emit('game-over');
+            io.emit("game-over");
           }
         } else {
           // Todas as fases foram concluídas
-          io.emit('game-over');
+          io.emit("game-over");
         }
       } else {
         // Exibe a próxima pergunta na fase atual
-        const nextQuestion = questionsForPhase[nextQuestionIndexes[currentPhase]];
-        io.emit('question', nextQuestion);
+        const nextQuestion =
+          questionsForPhase[nextQuestionIndexes[currentPhase]];
+        io.emit("question", nextQuestion);
       }
     } else {
       // Não há mais fases, emitir 'game-over'
-      io.emit('game-over');
+      io.emit("game-over");
     }
   });
 
-  socket.on('answer', ({ playerId, questionId, isCorrect }) => {
+  socket.on("answer", ({ playerId, questionId, isCorrect }) => {
     // Verifica se o jogador já respondeu a esta pergunta
     if (!data.answeredQuestions.includes(questionId)) {
       // Adiciona o ID da pergunta à lista de perguntas respondidas
       data.answeredQuestions.push(questionId);
-  
+
       // Verifica se o jogador está no objeto de participantes
       if (!data.participants[playerId]) {
         // Se não estiver, crie uma entrada para o jogador
@@ -175,70 +187,111 @@ io.on('connection', (socket) => {
           score: 0,
         };
       }
-  
+
       // Atualiza a pontuação do jogador com base na resposta
       if (isCorrect) {
         data.participants[playerId].score += 10;
       } else {
         data.participants[playerId].score -= 10;
       }
-  
+
       // Salva os dados em um arquivo JSON
       saveDataToJsonFile(data);
     }
   });
 
-  socket.on('joinRoom', (roomCode, studentId) => {
+  socket.on("joinRoom", (roomCode, studentId) => {
     // Verifica se o código da sala existe
     if (roomCode === room.roomCode) {
       socket.join(roomCode);
       if (!room.users) {
         room.users = [];
         room.users.push({ socketId: socket.id, studentId });
-        socket.emit('studentAuthenticated', 'Você foi autenticado com sucesso na sala.');
+        socket.emit(
+          "studentAuthenticated",
+          "Você foi autenticado com sucesso na sala."
+        );
       } else {
-        const userIsExisting = room.users.find((user) => user.socketId === socket.id);
-  
+        const userIsExisting = room.users.find(
+          (user) => user.socketId === socket.id
+        );
+
         if (!userIsExisting) {
           room.users.push({ socketId: socket.id, studentId });
           // Emite um evento de sucesso para o aluno
-          socket.emit('studentAuthenticated', 'Você foi autenticado com sucesso na sala.');
+          socket.emit(
+            "studentAuthenticated",
+            "Você foi autenticado com sucesso na sala."
+          );
         } else {
-          socket.emit('userIsExistingInTheRoom', 'Você não pode ter dois acessos simultâneos');
+          socket.emit(
+            "userIsExistingInTheRoom",
+            "Você não pode ter dois acessos simultâneos"
+          );
         }
       }
       console.log(`O estudante ${studentId} entrou na sala`);
       saveRoomToFile();
-  
-      io.emit('currentRoom', room);
+
+      io.emit("currentRoom", room);
     } else {
       // Emite um evento de erro para o aluno
-      socket.emit('roomError', 'A sala não existe ou o código está incorreto.');
+      socket.emit("roomError", "A sala não existe ou o código está incorreto.");
     }
   });
 
-  socket.on('authenticate', ({ role }) => {
-    if (role === 'presenter') {
+  socket.on("authenticate", ({ role }) => {
+    if (role === "presenter") {
       const roomCode = generateRoomCode();
       socket.join(roomCode);
-      socket.emit('roomCode', roomCode);
-  
+      socket.emit("roomCode", roomCode);
+
       // Salva código da sala em um JSON
       room.roomCode = roomCode;
       saveRoomToFile(room);
     }
   });
 
-  socket.on('initQuiz', () => {
+  /* ---------------------------------- New Version by Breno ---------------------------------- */
+  socket.on("init-quiz", () => {
     if (room.users.length > 0) {
-      io.emit('initGame', true);
+      const { nextQuestion, level } = initQuiz();
+      io.emit("show-next-question", { question: nextQuestion, level });
     } else {
-      socket.emit('initGameError', 'O jogo deve conter no minímo 1 jogador');
+      socket.emit("initGameError", "O jogo deve conter no minímo 1 jogador");
     }
-    
-  })
+  });
+
+  socket.on("foward", () => {
+    setNextQuestion();
+    const question = getCurrentQuestion();
+
+    if (question) {
+      const phase = getCurrentPhase();
+      io.emit("show-next-question", { question, level: phase.level });
+      return;
+    }
+
+    const { finishedGame } = updateToNextLevel();
+
+    io.emit("show-classification", {
+      clientPoints: getClientGameState(),
+      finishedGame,
+    });
+  });
+
+  socket.on("show-answer", () => {
+    io.emit("show-answer");
+    calculatePointsAndRestartUsersCurrentAnswers();
+  });
+
+  socket.on("user-answer", ({ socketId, answerId }) => {
+    if (!getCurrentUserAnswer(socketId)) {
+      io.emit("user-answer-to-presenter", { socketId });
+    }
+    updateUserAnswer({ socketId, answerId });
+  });
 });
-  
 
 // Inicializa o servidor na porta desejada
 const PORT = process.env.PORT || 4000;
---
+++ backend/usecases/game.js
@@ -0,0 +1,137 @@
+const phasesConfig = require("./database/phases_config.json");
+
+// Armazena fase e questão atual
+let currentPhase = {};
+let currentQuestion = {};
+
+// Armazena o estado da pontuação jogo para cada cliente
+let clientGameState = {};
+
+// Objeto para rastrear as salas
+// const room = require("./database/room.json") || { roomCode: "", users: [] };
+
+/* ---------------------------------- GAME POINTS CONTROL ---------------------------------- */
+
+// Respostas da pergunta atual, é um mapa = { [socketId]: { answerId: 0 } }
+let usersCurrentAnswers = {};
+
+function updateUserAnswer({ socketID, answerId }) {
+  usersCurrentAnswers[socketID] = {
+    answerId,
+  };
+}
+
+function calculatePointsAndRestartUsersCurrentAnswers() {
+  for (const key in usersCurrentAnswers) {
+    const currentAnswer = usersCurrentAnswers[key];
+    const isAnswerCorrect =
+      currentAnswer.answerId === currentQuestion.correct_answer_id;
+    const points = isAnswerCorrect
+      ? currentPhase.points_to_earn
+      : -currentPhase.points_to_lose;
+    clientGameState[key].points = clientGameState[key].points + points;
+  }
+
+  usersCurrentAnswers = {};
+}
+
+/* ---------------------------------- GAME FLUX CONTROL ---------------------------------- */
+
+function __createClientGameStateFromRoom() {
+  clientGameState = room.users.reduce(
+    (accumulator, currentValue) => ({
+      ...accumulator,
+      [currentValue.socketId]: {
+        ...currentValue,
+        points: 10,
+      },
+    }),
+    {}
+  );
+}
+
+function initQuiz() {
+  __createClientGameStateFromRoom();
+  const easyPhase = phasesConfig.easy;
+  const { questions, ...phaseConfig } = easyPhase;
+
+  const [nextQuestion, ...remainingQuestions] = questions;
+  currentPhase = {
+    level: "easy",
+    ...phaseConfig,
+    questions: remainingQuestions,
+  };
+
+  currentQuestion = nextQuestion;
+
+  return { nextQuestion, level: "easy" };
+}
+
+function setNextQuestion() {
+  const { questions, ...phaseConfig } = currentPhase;
+
+  if (questions.lenght === 0) {
+    currentQuestion = null;
+    return;
+  }
+
+  const [nextQuestion, ...remainingQuestions] = questions;
+  currentPhase = {
+    ...phaseConfig,
+    questions: remainingQuestions,
+  };
+
+  currentQuestion = nextQuestion;
+}
+
+function updateToNextLevel() {
+  const nextLevel = currentPhase.next_level;
+
+  if (nextLevel === "finish") {
+    return { finishedGame: true };
+  }
+
+  const nextLevelConfig = phasesConfig[currentPhase.next_level];
+  currentPhase = {
+    level: currentPhase.next_level,
+    ...nextLevelConfig,
+  };
+  currentQuestion = {};
+
+  return { finishedGame: false };
+}
+
+/* ---------------------------------- GETTERS ---------------------------------- */
+
+function getCurrentQuestion() {
+  return currentQuestion;
+}
+
+function getCurrentPhase() {
+  return currentPhase;
+}
+
+function getClientGameState() {
+  return clientGameState;
+}
+
+function getCurrentUserAnswer(socketId) {
+  return usersCurrentAnswers[socketId];
+}
+
+module.exports = {
+  // Game Points Control
+  calculatePointsAndRestartUsersCurrentAnswers,
+  updateUserAnswer,
+
+  // Game Flux Control
+  initQuiz,
+  setNextQuestion,
+  updateToNextLevel,
+
+  // Getters
+  getCurrentQuestion,
+  getCurrentPhase,
+  getClientGameState,
+  getCurrentUserAnswer,
+};
