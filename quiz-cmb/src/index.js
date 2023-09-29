7312ac613ab3734337d245de7b99924999f6796c
brenoxavier48 <brenoxavier8.bx@gmail.com>
2023-09-29 18:16:09 -0300

    feat: separate app file

--- quiz-cmb/src/index.js
+++ quiz-cmb/src/index.js
@@ -1,29 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 /**
  * Pages
  */
-import HomePresenter from './pages/HomePresenter';
-import HomeStudent from './pages/HomeStudent';
-import WaitingRoom from './pages/WaitingRoom';
-import QuestionStudent from './pages/QuestionStudent';
-import QuestionPresenter from './pages/QuestionPresenter';
-import Classification from './pages/Classification';
-import Eliminated from './pages/Eliminated';
+import App from "./app.js";
 
-ReactDOM.render(
-  <Router>
-    <Routes>
-      <Route path="/home/presenter" element={<HomePresenter />} />
-      <Route path="/home/student" element={<HomeStudent />} />
-      <Route path="/waiting-room" element={<WaitingRoom />} />
-      <Route path="/question/student" element={<QuestionStudent />} />
-      <Route path="/question/presenter" element={<QuestionPresenter />} />
-      <Route path="/classification" element={<Classification />} />
-      <Route path="/eliminated" element={<Eliminated />} />
-    </Routes>
-  </Router>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
