fe38ed68b9642a00854666b3ff1226818d7e8286
Victor Junior <64840291+victorjunior95@users.noreply.github.com>
2023-09-30 12:07:33 -0300

    tradução da string da fase na página de questão do estudante

--- quiz-cmb/src/pages/QuestionStudent/components/TimeQuestion/index.jsx
+++ quiz-cmb/src/pages/QuestionStudent/components/TimeQuestion/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './styles.css';
 
+
+
+
+
+
 function TimeQuestion({ currentLevel }) {
+  let currentLevelPT = "fácil"
+  currentLevel === "medium" ? currentLevelPT = "média" : currentLevelPT = "difícil"
+
   return (
     // <div className="time-question-student">
     //   <div>30</div>
     // </div>
     <div className="level-student" >
-      <div className="current-level-student">{currentLevel}</div>
+      <div className="current-level-student">{currentLevelPT}</div>
     </div>
   );
 }
