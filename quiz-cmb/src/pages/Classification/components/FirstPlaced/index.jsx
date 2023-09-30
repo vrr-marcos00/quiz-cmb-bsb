9a90be45cf71ae2cc58dd8ef29632268ce1dc982
brenoxavier48 <brenoxavier8.bx@gmail.com>
2023-09-30 05:48:25 -0300

    feat: add classification event

--- quiz-cmb/src/pages/Classification/components/FirstPlaced/index.jsx
+++ quiz-cmb/src/pages/Classification/components/FirstPlaced/index.jsx
@@ -1,100 +1,29 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import "./styles.css";
 
 /**
  * Images
  */
-import IMAGE_CMB from '../../../../assets/images/CMB.png';
-import IMAGE_CMBEL from '../../../../assets/images/CMBel.png';
-import IMAGE_CMBH from '../../../../assets/images/CMBH.png';
-import IMAGE_CMC from '../../../../assets/images/CMC.png';
-import IMAGE_CMCG from '../../../../assets/images/CMCG.png';
-import IMAGE_CMM from '../../../../assets/images/CMM.png';
-import IMAGE_CMPA from '../../../../assets/images/CMPA.png';
-import IMAGE_CMR from '../../../../assets/images/CMR.png';
-import IMAGE_CMRJ from '../../../../assets/images/CMRJ.png';
-import IMAGE_CMSM from '../../../../assets/images/CMSM.png';
-import IMAGE_CMSP from '../../../../assets/images/CMSP.png';
-import IMAGE_CMVM from '../../../../assets/images/CMVM.png';
+import IMAGE_CMB from "../../../../assets/images/CMB.png";
+import IMAGE_CMBEL from "../../../../assets/images/CMBel.png";
+import IMAGE_CMBH from "../../../../assets/images/CMBH.png";
+import IMAGE_CMC from "../../../../assets/images/CMC.png";
+import IMAGE_CMCG from "../../../../assets/images/CMCG.png";
+import IMAGE_CMM from "../../../../assets/images/CMM.png";
+import IMAGE_CMPA from "../../../../assets/images/CMPA.png";
+import IMAGE_CMR from "../../../../assets/images/CMR.png";
+import IMAGE_CMRJ from "../../../../assets/images/CMRJ.png";
+import IMAGE_CMSM from "../../../../assets/images/CMSM.png";
+import IMAGE_CMSP from "../../../../assets/images/CMSP.png";
+import IMAGE_CMVM from "../../../../assets/images/CMVM.png";
 
-
-function FisrtPlaced() {
-
-  const arrayColleges = ["CMB", "CMBEL", "CMBH", "CMC", "CMCG", "CMF", "CMM", "CMPA", "CMR", "CMRJ", "CMSM", "CMSP", "CMVM", "CMVM0", "CMVM1", "CMVM2"];
-
-  const classification = [
-    {
-      name: "CMB",
-      points: 15,
-    },
-    {
-      name: "CMBEL",
-      points: 18,
-    },
-    {
-      name: "CMBH",
-      points: 20,
-    },
-    {
-      name: "CMC",
-      points: 22,
-    },
-    {
-      name: "CMCG",
-      points: 2,
-    },
-    {
-      name: "CMF",
-      points: 2,
-    },
-    { 
-      name: "CMM",
-      points: 5,
-    },
-    {
-      name: "CMPA",
-      points: 5,
-    },
-    {
-      name: "CMR",
-      points: 70,
-    },
-    {
-      name: "CMRJ",
-      points: 50,
-    },
-    {
-      name: "CMSM",
-      points: 5,
-    },
-    {
-      name: "CMSP",
-      points: 2,
-    },
-    {
-      name: "CMVM",
-      points: 21,
-    },
-    {
-      name: "CMVM0",
-      points: 12
-    },
-    {
-      name: "CMVM1",
-      points: 30
-    },
-    {
-      name: "CMVM2",
-      points: 11
-    }
-  ];
-
+function FisrtPlaced({ classification }) {
   const sortPlayers = classification.sort((a, b) => {
     return b.points - a.points;
   });
 
   const images_colleges = {
-    CMB: { image: IMAGE_CMB},
+    CMB: { image: IMAGE_CMB },
     CMBEL: { image: IMAGE_CMBEL },
     CMBH: { image: IMAGE_CMBH },
     CMC: { image: IMAGE_CMC },
@@ -109,31 +38,37 @@ function FisrtPlaced() {
     CMVM: { image: IMAGE_CMVM },
     CMVM0: { image: IMAGE_CMVM },
     CMVM1: { image: IMAGE_CMVM },
-    CMVM2: { image: IMAGE_CMVM }
-  }
+    CMVM2: { image: IMAGE_CMVM },
+  };
 
   const addClassThreeFirsts = ["first-player", "second-player", "third-player"];
 
   return (
-    < div className='first-placed-row'>
+    <div className="first-placed-row">
       <span className="classification-title">Classificação</span>
       {sortPlayers.map((player, index) => {
         return (
-          <div className={`first-placed ${addClassThreeFirsts[index]}`} key={index}>
+          <div
+            className={`first-placed ${addClassThreeFirsts[index]}`}
+            key={index}
+          >
             <div className="first-placed-position">
               <span>{index + 1}º</span>
             </div>
             <div className="first-placed-college">
-              <img src={images_colleges[player.name].image} alt={player.name} />
-              <span>{player.name}</span>
+              <img
+                src={images_colleges[player.studentId].image}
+                alt={player.studentId}
+              />
+              <span>{player.studentId}</span>
             </div>
             <div className="first-placed-points">
               <span>{player.points} pts</span>
             </div>
           </div>
-        )
+        );
       })}
-    </div >
+    </div>
   );
 }
 
--- quiz-cmb/src/pages/Classification/index.jsx
+++ quiz-cmb/src/pages/Classification/index.jsx
@@ -1,17 +1,46 @@
 import React from "react";
 import "./styles.css";
 
+import { useNavigate } from "react-router-dom";
+
 /**
  * Components
  */
 import FirstPlaced from "./components/FirstPlaced";
 
 function Classification({ socket }) {
+  const navigate = useNavigate();
+  const { classification, finishedGame } = JSON.parse(
+    localStorage.getItem("classification")
+  );
+
+  const isPresenter = localStorage.getItem("isPresenter");
+
+  React.useEffect(() => {
+    socket.on("show-next-question", ({ question, level }) => {
+      localStorage.setItem(
+        "currentQuestion",
+        JSON.stringify({ question, level })
+      );
+      navigate(`/question/${isPresenter ? "presenter" : "student"}`);
+    });
+  }, []);
+
+  const handleFowardButtonClick = () => {
+    socket.emit("foward");
+  };
+
   return (
     <>
       <div className="container-page-classification">
+        <div className="row-main_buttons">
+          {isPresenter && !finishedGame && (
+            <button onClick={handleFowardButtonClick}>Avançar</button>
+          )}
+          {finishedGame && <h1>Jogo Finalizado</h1>}
+        </div>
         <div className="firt-classified">
-          <FirstPlaced />
+          <FirstPlaced classification={classification} />
         </div>
       </div>
     </>
